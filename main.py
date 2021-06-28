1bce398bf4d87a52bd2aba4e70b922bdaff82189
Leon Minasyan <ierehon1905@gmail.com>
2021-06-28 19:00:20 +0300

    добпвил и нструкцию

--- main.py
+++ main.py
@@ -18,5 +18,6 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+# Чтобы запустить программу испоолните poetry run python main.py
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
